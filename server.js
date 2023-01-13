f9bea782831cf5d98f05e6a34762f11e986514b8
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2023-01-13 13:59:02 -0500

    cors removed

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 require('dotenv').config()
 const path = require("path")
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const PORT = 3001;
 const router = require("./router/post");
 const connectDatabase = require('./config/database');
-// require("./config/database")
-
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
+app.use(express.static(path.join(__dirname, "build")))
 
 connectDatabase()
 
